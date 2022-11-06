392d8713aa40a0faf0c800c0d94e5f3cda9eb89d
jorj-kare <jorj.kare@gmail.com>
2022-11-06 19:07:39 +0100

    feat: add delete my account

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
+const { findOne } = require("../models/userModel");
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 exports.createNewUser = catchAsync(async (req, res, next) => {
@@ -12,5 +14,20 @@ exports.createNewUser = catchAsync(async (req, res, next) => {
 
   res.status(201).json({
     status: "success",
+  });
+});
+exports.deleteMyAccount = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return next(new AppError("You must provide an email and a password", 404));
+  const user = await User.findOne({ email }).select("+password");
+  console.log(user);
+
+  if (!user || !(await user.isPasswordCorrect(password, user.password)))
+    return next(new AppError("Wrong email or password, please try again", 404));
+  await User.findByIdAndDelete(user._id);
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });
--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -4,4 +4,5 @@ const userController = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/signup/").post(userController.createNewUser);
+router.route("/deleteMyAccount").delete(userController.deleteMyAccount);
 module.exports = router;
